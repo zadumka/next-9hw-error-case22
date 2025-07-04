c035cb2203b3443abfa8de2bded0785d2aec76cf
zadumka <90329284+zadumka@users.noreply.github.com>
2025-07-04 20:05:22 +0300

    Update page.tsx

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { AuthRequest, register } from '@/lib/api/clientApi';
+import { AuthRequest } from '@/lib/api/clientApi';
 import { useAuthStore } from '@/lib/store/authStore';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-
 import css from './SignUp.module.css';
 
 export const dynamic = 'force-static';
@@ -12,19 +11,15 @@ export const dynamic = 'force-static';
 export default function SignUp() {
   const router = useRouter();
   const [error, setError] = useState('');
-
   const setUser = useAuthStore((state) => state.setUser);
 
   const handleSubmit = async (formData: FormData) => {
     try {
       const formValues = Object.fromEntries(formData) as AuthRequest;
-      const user = await register(formValues);
-      if (user) {
-        setUser(user);
-        router.replace('/profile');
-      } else {
-        setError('Invalid email or password');
-      }
+      
+      const mockUser = { id: '1', email: formValues.email, username: 'test' };
+      setUser(mockUser);
+      router.replace('/profile');
     } catch (error) {
       console.log('error', error);
       setError('Oops... some error');
@@ -39,12 +34,10 @@ export default function SignUp() {
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required className={css.input} />
         </div>
-
         <div className={css.formGroup}>
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" required className={css.input} />
         </div>
-
         <div className={css.actions}>
           <button type="submit" className={css.submitButton}>
             Register
